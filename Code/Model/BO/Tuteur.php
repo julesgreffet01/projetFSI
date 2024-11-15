f0f46d33373aae34d59a7ae11b6b935abf2e69cf
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2024-11-15 11:29:23 +0100

    jules

--- Code/Model/BO/Tuteur.php
+++ Code/Model/BO/Tuteur.php
@@ -8,6 +8,7 @@ class Tuteur extends Utilisateur
     private int $nbMax3;
     private int $nbMax4;
     private int $nbMax5;
+    private array $mesEtu;
 
     public function __construct(int $nbMax3, int $nbMax4, int $nbMax5, int $idUti, string $logUti, string $mdpUti, string $mailUti, string $telUti, string $nomUti, string $preUti, string $vilUti, string $adrUti, string $cpUti) {
         parent::__construct( $idUti, $logUti, $mdpUti, $mailUti, $telUti, $nomUti, $preUti, $vilUti, $adrUti, $cpUti);
@@ -15,6 +16,7 @@ class Tuteur extends Utilisateur
         $this->nbMax3 = $nbMax3;
         $this->nbMax4 = $nbMax4;
         $this->nbMax5 = $nbMax5;
+        $this->mesEtu = [null];
     }
 
 
@@ -50,7 +52,15 @@ class Tuteur extends Utilisateur
         $this->nbMax5 = $nbMax5;
     }
 
+    public function getMesEtu(): array
+    {
+        return $this->mesEtu;
+    }
 
+    public function setMesEtu(array $mesEtu): void
+    {
+        $this->mesEtu = $mesEtu;
+    }
 
 
 }
