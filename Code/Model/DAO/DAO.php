e41bb68b6b418cd065b52f9098991f1dc053e5be
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2024-11-15 09:44:29 +0100

    jules crea DAO

---
+++ Code/Model/DAO/DAO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DAO;
+use PDO;
+
+abstract class DAO
+{
+    protected PDO $bdd;
+
+    public function __construct(PDO $bdd) {
+        $this->bdd = $bdd;
+    }
+
+    public abstract function create(object $obj): bool;
+
+    public abstract function update(object $obj): bool;
+
+    public abstract function delete(object $obj): bool;
+
+    public abstract function find(int $id): ?object;
+
+    public abstract function getAll(): array;
+
+
+}
