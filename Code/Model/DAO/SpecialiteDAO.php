2b10a31daff5b509b3bee59411d8e612f26bb482
Jules Greffet <julesgreffet@gmail.com>
2024-11-18 22:37:01 +0100

    jules debut DAO

--- Code/Model/DAO/SpecialiteDAO.php
+++ Code/Model/DAO/SpecialiteDAO.php
@@ -1,7 +1,7 @@
 <?php
 namespace DAO;
+use BO\Specialite;
 use PDO;
-use DAO\DAO;
 
 require_once 'DAO.php';
 
@@ -12,7 +12,21 @@ class SpecialiteDAO extends DAO
 
     public function create(object $obj): bool
     {
-        return false;
+        $result = false;
+        if ($obj->getId()!= $this->find($obj->getId())) {
+            $query = "INSERT INTO Specialite (Nom_Spe) values (:libSpe)";
+            $stmt = $this->bdd->prepare($query);
+            $r = $stmt -> execute(
+            [
+                ":libSpe"=>$obj->getNom(),
+            ]
+            );
+            if($r !== false){
+                $result = true;
+            }
+
+        }
+        return $result;
     }
 
     public function update(object $obj): bool
@@ -27,11 +41,34 @@ class SpecialiteDAO extends DAO
 
     public function find(int $id): ?object
     {
-        return null;
+        $result = null;
+        $query = "SELECT * FROM Specialite WHERE id = :id";
+        $stmt = $this->bdd->prepare($query);
+        $r = $stmt -> execute(
+            [
+                ":id"=>$id
+            ]
+        );
+        if($r !== false){
+            $row = ($tmp = $stmt->fetch(PDO::FETCH_ASSOC)) ? $tmp : null;       //permet de dire que si il prens pas de valeur le mettre null
+            if (!is_null($row)) {
+                $result = new Specialite($row['IdSpe'], $row['NomSpe']);
+            }
+        }
+        return $result;
     }
 
     public function getAll(): array
     {
-        return [null];
+        $result = [null];
+        $query = "SELECT * FROM Specialite";
+        $stmt = $this->bdd->prepare($query);
+        if($stmt){
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            foreach ($stmt as $row){
+                $result[] = new Specialite($row['IdSpe'], $row['NomSpe']);
+            }
+        }
+        return $result;
     }
 }
---
+++ Code/Test/testBO.php
@@ -0,0 +1,13 @@
+<?php
+
+use BO\Specialite;
+
+require_once "../Model/BO/Specialite.php";
+
+
+
+$spec = new Specialite(1, "slam");
+echo $spec->getIdSpec();
+echo "<br>";
+echo $spec->getNomSpec();
+var_dump($spec);
