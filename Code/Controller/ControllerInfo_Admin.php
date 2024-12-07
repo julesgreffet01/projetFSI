bba58ec3655f598c8e427d45b6e5624f0d5f521f
Jules Greffet <julesgreffet@gmail.com>
2024-12-07 14:21:13 +0100

    jules

--- Code/Controller/ControllerInfo_Admin.php
+++ Code/Controller/ControllerInfo_Admin.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+require_once __DIR__."/../Model/BO/Administrateur.php";
+require_once __DIR__."/../Model/BO/Etudiant.php";
+require_once __DIR__."/../Model/BO/Tuteur.php";
+require_once __DIR__."/../Model/BO/Specialite.php";
+require_once __DIR__."/../Model/BO/Classe.php";
+
+
 $titrefichier = "Accueil";
 $stylecss = "Blockinfo.css";
 
