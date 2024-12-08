a01d9ceb18c4a446021105f3dd06e4459df0876c
Jules Greffet <julesgreffet@gmail.com>
2024-12-08 18:31:43 +0100

    jules

--- Code/Controller/ControllerInfo_Etudiant.php
+++ Code/Controller/ControllerInfo_Etudiant.php
@@ -1,5 +1,8 @@
 <?php
 
+use BO\Administrateur;
+use BO\Etudiant;
+
 session_start();
 
 
@@ -20,48 +23,49 @@ require_once  __DIR__ ."/../Model/BO/Classe.php";
 $titrefichier = "Accueil";
 $stylecss = "Blockinfo.css";
 
+if (unserialize($_SESSION['utilisateur']) instanceof Etudiant) {
+    $etu = unserialize($_SESSION['utilisateur']);
 
-$etu = unserialize($_SESSION['utilisateur']);
-
-$nom = $etu->getNomUti();
-$pre = $etu->getPreUti();
-$tel = $etu->getTelUti();
-$adr = $etu->getAdrUti();
-$mail = $etu->getMailUti();
-if ($etu->getMaClasse()){
-    $cla = $etu->getMaClasse()->getLibCla();
-} else {
-    $cla = "Pas assigné(e)";
-}
-if ($etu->getMaSpec()){
-    $spec = $etu->getMaSpec()->getNomSpec();
-} else {
-    $spe = "Pas assigné(e)";
-}
-
-if ($etu->getMonEnt()){
-    $ent = $etu->getMonEnt()->getNomEnt();
-    $adrEnt = $etu->getMonEnt()->getAdrEnt();
-    if ($etu->getMonMaitreAp()){
-        $nomMai = $etu->getMonMaitreAp()->getNomMai();
-        $preMai = $etu->getMonMaitreAp()->getPreMai();
-        $telMai = $etu->getMonMaitreAp()->getTelMai();
-        $mailMai = $etu->getMonMaitreAp()->getMailMai();
+    $nom = $etu->getNomUti();
+    $pre = $etu->getPreUti();
+    $tel = $etu->getTelUti();
+    $adr = $etu->getAdrUti();
+    $mail = $etu->getMailUti();
+    if ($etu->getMaClasse()){
+        $cla = $etu->getMaClasse()->getLibCla();
     } else {
+        $cla = "Pas assigné(e)";
+    }
+    if ($etu->getMaSpec()){
+        $spec = $etu->getMaSpec()->getNomSpec();
+    } else {
+        $spe = "Pas assigné(e)";
+    }
+
+    if ($etu->getMonEnt()){
+        $ent = $etu->getMonEnt()->getNomEnt();
+        $adrEnt = $etu->getMonEnt()->getAdrEnt();
+        if ($etu->getMonMaitreAp()){
+            $nomMai = $etu->getMonMaitreAp()->getNomMai();
+            $preMai = $etu->getMonMaitreAp()->getPreMai();
+            $telMai = $etu->getMonMaitreAp()->getTelMai();
+            $mailMai = $etu->getMonMaitreAp()->getMailMai();
+        } else {
+            $nomMai = "Pas assigné(e)";
+            $preMai = "";
+            $telMai = "";
+            $mailMai = "";
+        }
+    } else {
+        $ent = "Pas assigné(e)";
+        $adrEnt = "";
         $nomMai = "Pas assigné(e)";
         $preMai = "";
         $telMai = "";
         $mailMai = "";
     }
+    include_once ('../View/Nav_Bar.php');
+    include_once ('../View/Page_Info_Etudiant.php');
 } else {
-    $ent = "Pas assigné(e)";
-    $adrEnt = "";
-    $nomMai = "Pas assigné(e)";
-    $preMai = "";
-    $telMai = "";
-    $mailMai = "";
+    header('Location: ControllerConnexion.php');
 }
-
-
-include_once ('../View/Nav_Bar.php');
-include_once ('../View/Page_Info_Etudiant.php');
