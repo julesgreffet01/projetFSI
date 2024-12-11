ba6451420a4a12a60e3b9218f71cb40e65a77fed
Jules Greffet <julesgreffet@gmail.com>
2024-12-11 21:10:55 +0100

    jules

--- Code/View/Page_Parametre_Etudiant.php
+++ Code/View/Page_Parametre_Etudiant.php
@@ -1,27 +1,49 @@
+<form action="ControllerParametre_Etudiant.php">
 <div class="centre">
     <p>
-        <label for="city">Prénom</label>
-        <input type="text" class="typetext" name="city" value="" placeholder="" />
+        <label for="preEtu">Prénom</label>
+        <input type="text" class="typetext" name="preEtu" value="" placeholder="" id="preEtu"/>
     </p>
     <p>
-        <label for="city">Nom</label>
-        <input type="text" class="typetext" name="city" value="" placeholder="" />
+        <label for="nomEtu">Nom</label>
+        <input type="text" class="typetext" name="nomEtu" value="" placeholder="" id="nomEtu"/>
     </p>
     <p>
-        <label for="city">Téléphone</label>
-        <input type="text" class="typetext" name="city" value="" placeholder="" />
+        <label for="telEtu">Téléphone</label>
+        <input type="text" class="typetext" name="telEtu" value="" placeholder="" id="telEtu"/>
     </p>
     <p>
-        <label for="city">Adresse</label>
-        <input type="text" class="typetext" name="city" value="" placeholder="" />
+        <label for="adrEtu">Adresse</label>
+        <input type="text" class="typetext" name="adrEtu" value="" placeholder="" id="adrEtu"/>
     </p>
     <p>
-        <label for="city">Mail</label>
-        <input type="text" class="typetext" name="city" value="" placeholder="" />
+        <label for="vilEtu">Ville</label>
+        <input type="text" class="typetext" name="vilEtu" value="" placeholder="" id="vilEtu"/>
     </p>
+    <p>
+        <label for="cpEtu">Code Postale</label>
+        <input type="text" class="typetext" name="cpEtu" value="" placeholder="" id="cpEtu"/>
+    </p>
+    <p>
+        <label for="mailEtu">Mail</label>
+        <input type="text" class="typetext" name="mailEtu" value="" placeholder="" id="mailEtu"/>
+    </p>
+    <p>
+        <label for="logEtu">Log</label>
+        <input type="text" class="typetext" name="logEtu" value="" placeholder="" id="logEtu"/>
+    </p>
+    <p>
+        <label for="mdpEtu">Mot de passe</label>
+        <input type="text" class="typetext" name="mdpEtu" value="" placeholder="" id="mdpEtu"/>
+    </p>
+    <p>
+        <label for="altEtu">Alternance</label>
+        <input type="checkbox" class="typecheck" name="altEtu" value="true" id="altEtu"/>
+    </p>
+
     <p>
         <label for="class-select">Classe</label>
-        <select name="class-select">
+        <select name="class-select" id="class-select">
             <option value=""></option>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
@@ -32,8 +54,8 @@
         </select>
     </p>
     <p>
-        <label for="city">Spécialisation</label>
-        <select name="maitre-select">
+        <label for="spec-select">Spécialisation</label>
+        <select name="spec-select" id="spec-select">
             <option value=""></option>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
@@ -44,8 +66,8 @@
         </select>
     </p>
     <p>
-        <label for="city">Nom de l'entreprise</label>
-        <select name="maitre-select">
+        <label for="ent-select">Nom de l'entreprise</label>
+        <select name="ent-select" id="ent-select">
             <option value=""></option>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
@@ -56,8 +78,8 @@
         </select>
     </p>
     <p>
-        <label for="city">Nom du maître d'apprentissage</label>
-        <select name="maitre-select">
+        <label for="maitre-select">Nom du maître d'apprentissage</label>
+        <select name="maitre-select" id="maitre-select">
             <option value=""></option>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
@@ -68,8 +90,8 @@
         </select>
     </p>
     <p>
-        <label for="city">Nom et prénom du Tuteur</label>
-        <select name="maitre-select">
+        <label for="tut-select">Nom et prénom du Tuteur</label>
+        <select name="tut-select" id="tut-select">
             <option value=""></option>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
@@ -82,8 +104,8 @@
 </div>
 <div class="selectetudiant">
 <div class="selection">
-    <label for="city">Etudiant à modifier</label>
-    <select name="tuteur-select">
+    <label for="etu-select">Etudiant à modifier</label>
+    <select name="etu-select" id="dropDown">
         <option value=""></option>
         <option value="dog">Dog</option>
         <option value="cat">Cat</option>
@@ -99,3 +121,4 @@
     <input type="submit" name="btnUpdate" value="Modifier" class="btnbleu"/>
     <input type="submit" name="btnDelete" value="Supprimer" class="btnrouge"/>
 </div>
+</form>
