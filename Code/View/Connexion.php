f51cbb4b22be98e93d7cd57493100b42e99d13a1
Maxirrx <max2005.roux@gmail.com>
2024-12-11 18:13:52 +0100

    commit

--- Code/View/Connexion.php
+++ Code/View/Connexion.php
@@ -6,9 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="../Css/Style_Connexion.css" rel="stylesheet" />
+    <link href="../Css/Bouton.css" rel="stylesheet" />
     <title>Connexion</title>
 </head>
 <body>
+<div class="centre">
 <div class="contenu">
     <div class="vert">
         <img class="logo" src="../Img/logo.jpeg">
@@ -33,15 +35,16 @@
             <h1>Connexion</h1>
         </div>
         <form action="../Controller/ControllerConnexion.php" method="POST">
+            <div class="mdpou">
             <input type="text" class="libelle" name="log">
-            <input type="text" class="libelle" name="mdp">
-            <div class="mdp">
-            <a href="#">Mot de passe oublié</a>
+            <input type="password" class="libelle" name="mdp">
+
             </div>
-            <div class="boutton">
+
+
 <!--                <a href="Accueil_Admin.php">Connexion</a>-->
-            <input type="submit" value="Connexion" name="btnConnexion">
-            </div>
+            <input type="submit" class="btnbil" value="Connexion" name="btnConnexion">
+
         </form>
 
         <?php if ($errorMessage != ""){
@@ -51,6 +54,7 @@
 
         <!--        mettre le message d erreur quelque part qui se nomme $errorMessage et qui renvoie si tous les champs ne sont pas remplie ou si le log ou mdp sont faux-->
     </div>
+</div>
 </div>
 </body>
 </html>
