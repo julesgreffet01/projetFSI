eebd2d346a80c9bd9d215ffd1f51af20ee89976f
Jules Greffet <julesgreffet@gmail.com>
2024-12-22 00:18:40 +0100

    jules

--- Code/Controller/ControllerModif_Bilan1.php
+++ Code/Controller/ControllerModif_Bilan1.php
@@ -1,4 +1,10 @@
 <?php
+
+use BO\Administrateur;
+use BO\Bilan;
+use BO\Tuteur;
+use DAO\Bilan1DAO;
+
 session_start();
 
 require_once __DIR__."/../Model/DAO/AdministrateurDAO.php";
@@ -14,6 +20,18 @@ require_once __DIR__."/../Model/BO/Tuteur.php";
 $titrefichier = "Modifier mon bilan";
 $stylecss = "Blockinfo.css";
 $stylecss3 = "Bouton.css";
+$bdd = initialiseConnexionBDD();
+$uti = unserialize($_SESSION['utilisateur']);
 
+if($uti instanceof Administrateur || $uti instanceof Tuteur){
+        $idBil = intval($_GET['id']);
+        $bil1DAO = new Bilan1DAO($bdd);
+        $bil1 = $bil1DAO->find($idBil);
+        if(isset($_POST['btnValid']) && $_POST['libBil'] != '' && $_POST['datVisEnt'] != '' && $_POST['notEnt'] != '' && $_POST['notOra'] != '' && $_POST['notDos'] != ''){
+            //TODO finir la partie modif
+        }
+} else {
+    header('Location: ControllerConnexion.php');
+}
 include_once ('../View/Nav_Bar.php');
 include_once ('../View/Modif_Bilan1.php');
--- Code/View/Modif_Bilan1.php
+++ Code/View/Modif_Bilan1.php
@@ -1,33 +1,33 @@
 <div class="centreinf">
 <div class="block">
-    <form action="Bilan1.php" method="post">
-    <p>
+    <form action="" method="post">
+        <p>
+            <label class="intitu">Libellé du bilan</label>
+            <input type="text" name="libBil" class="info" value="<?php echo $bil1->getLibBil() ? $bil1->getLibBil() : '' ?>">
+        </p>
+        <p>
         <label class="intitu">Date de la visite en entreprise</label>
-        <input type="text" class="info">
-    </p>
-    <p>
-        <label class="intitu">Date du Bilan</label>
-        <input type="text" class="info">
+        <input type="date" name="datVisEnt" class="info" value="<?php echo $bil1->getDatVisEnt() ? $bil1->getDatVisEnt()->format('Y-m-d') : ''; ?>">
     </p>
     <p>
         <label class="intitu">Note fixée par l'entreprise</label>
-        <input type="text" class="info">
+        <input type="text" name="notEnt" class="info" value="<?php echo $bil1->getNotEnt() ? $bil1->getNotEnt() : '' ?>">
     </p>
     <p>
         <label class="intitu">Note d'oral</label>
-        <input type="text" class="info">
+        <input type="text" name="notOra" class="info" value="<?php echo $bil1->getNotOra() ? $bil1->getNotOra() : '' ?>">
     </p>
     <p>
         <label class="intitu">Note du dossier</label>
-        <input type="text" class="info">
+        <input type="text" name="notDos" class="info" value="<?php echo $bil1->getNotBil() ? $bil1->getNotBil() : '' ?>">
     </p>
     <p>
         <label class="intitu">Remarque éventuelles</label>
-        <input type="text" class="info">
+        <textarea class="info" name="remBil"><?php echo $bil1->getRemBil() ? $bil1->getRemBil() : '' ?></textarea>
     </p>
         <div class="btnbila">
-    <input type="button" class="btnbleu" value="Valider">
-    <input type="button" class="btnrouge" value="Annuler">
+    <input type="submit" class="btnbleu" value="Valider" name="btnValid">
+    <input type="submit" class="btnrouge" value="Annuler" name="btnCancel">
         </div>
     </form>
 </div>
